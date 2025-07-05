35dbba401a9999171507716a51174d75df06e0ff
Angry Coding <mailidanielifechukwu@gmail.com>
2025-07-05 12:09:11 +0000

    chore: fixes

--- src/components/assets/ElizaLogo.tsx
+++ src/components/assets/ElizaLogo.tsx
@@ -1,8 +1,6 @@
 import clsx from "clsx";
 import Image from "next/image";
 
-import logo from "@/components/logo.png";
-
 interface LogoProps {
   width?: number;
   height?: number;
@@ -13,7 +11,7 @@ export function Logo({ width = 120, height = 32, className = "" }: LogoProps) {
   return (
     <div className={clsx(["select-none", className])}>
       <Image
-        src={logo}
+        src={"./logo.png"}
         alt="Eliza Logo"
         width={width}
         height={height}
